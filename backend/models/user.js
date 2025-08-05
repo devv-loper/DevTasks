7eec6e6218d692d33400e846160d901fdac31df5
Devv Sonii <devpsoni123@gmail.com>
2025-08-06 00:00:42 +0530

    added database schemas to models

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,9 +4,22 @@ const Schema = mongoose.Schema;
 
 
 const userSchema = new Schema({
-    email:{type: String, unique: true, required: true},
-    userName: {type: String, unique: true, required: true},
-    password: {type: String, required: true}
+    email:{
+        type: String,
+        unique: true,
+        required: true
+    },
+
+    userName:{
+        type: String,
+        unique: true,
+        required: true
+    },
+
+    password:{
+        type: String, 
+        required: true
+    }
 })
 
 const userModel = mongoose.model("User", userSchema);
