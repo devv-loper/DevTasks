4ce472404a6de411f4cf37659d9d1185038f9864
Devv Sonii <devpsoni123@gmail.com>
2025-08-14 22:34:31 +0530

    leaderBoard routes modified

--- backend/routes/leaderBoard.js
+++ backend/routes/leaderBoard.js
@@ -1,48 +1,64 @@
 const { Router } = require("express");
-const { authMiddleware } = require("../middleware/requireAuth");
-const { leaderBoardModel } = require("../models/leaderBoard");
-const user = require("./user");
+const  requireAuth  = require("../middleware/requireAuth");
+const leaderBoardModel = require("../models/leaderBoard");
+const {userModel} = require("../models/user");
 
-const leaderBoardRouter = { Router };
+const leaderBoardRouter = Router();
 
 
-leaderBoardRouter.get("/user/:user", authMiddleware, async(req, res) => {
+leaderBoardRouter.get("/user/:userId", requireAuth, async(req, res) => {
     try{
-        const userId = req.userId;
+        const { userId } = req.params;
         const leaderBoard = await userModel.findById(userId);
 
         if(!leaderBoard){
-            return res.status(404).json({
+            return res.status(400).json({
                 message: "User not found!"
             })
         } res.status(200).json(leaderBoard)
     } catch(error){
-        res.status(500).json({message:"Internal server error", error})
+        return res.status(500).json({message:"Internal server error", error})
     }
 
 })
-leaderBoardRouter.get("/XP", authMiddleware, async (req, res) => {
+leaderBoardRouter.get("/XP", requireAuth, async (req, res) => {
     try{
-        const totalXP = req.body;
-        const userXP = await leaderBoardModel.find(totalXP);
-        if(!userXP){
+        const userXP = await leaderBoardModel.find({userId: req.userId});
+        if(!userXP || userXP.length === 0){
             return res.status(400).json({message:"XP not available"})
         }
+        return res.status(200).json(userXP)
     } catch(error){
-        res.status(500).json({message:"Internal server error", error})
+        return res.status(500).json({message:"Internal server error", error})
     }
 })
 
-leaderBoardRouter.get("/type/:type", authMiddleware, async (req, res) => {
+leaderBoardRouter.get("/type/:type", requireAuth, async (req, res) => {
     try{
+        const { type } = req.params;
+        const validTypes = ['dsa', 'competitiveProgramming', 'development', 'github'];
+        if(!validTypes.includes(type.toLowerCase())){
+            return res.status(400).json({
+                message:"Not a valid type"
+            })
+        }
 
+        const LeaderBoard = await leaderBoardModel.find({userId: req.userId, type});
+        if(!LeaderBoard || LeaderBoard.length === 0){
+            return res.status(400).json({
+                message:"No leaderBoard Data found for this type"
+            });
+        }
+        res.status(200).json(LeaderBoard)
     } catch(error){
-        
+        return res.status(500).json({
+            message:"Internal Server Error", error
+        })
     }
 })
 
 
 module.exports = {
-    progressRouter: progressRouter
+    leaderBoardRouter: leaderBoardRouter
 }
 
